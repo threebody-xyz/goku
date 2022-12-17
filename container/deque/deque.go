b3397615407e051ce61fd8ad232a9f9b929144cb
shiyicode <shiyi@threebody.xyz>
2022-12-17 19:03:31 +0800

    Init

---
+++ container/deque/deque.go
@@ -0,0 +1,44 @@
+package deque
+
+type Element interface {}
+
+type Deque struct {
+}
+
+// PushFront inserts a new element e with value v at the front of deque d.
+func (d *Deque) PushFront(v Element)  {
+}
+
+// PushBack inserts a new element e with value v at the back of deque d.
+func (d *Deque) PushBack(v Element)  {
+}
+// Front returns the first element of deque d or nil if the deque is empty.
+func (d *Deque) Front() Element {
+	return nil
+}
+
+// Back returns the last element of deque d or nil if the deque is empty.
+func (d *Deque) Back() Element {
+	return nil
+}
+
+func (d *Deque) Get(i int) Element {
+	return nil
+}
+
+func (d *Deque) Set(i int, v Element) {
+}
+
+// Len returns the number of elements of deque d.
+func (d *Deque) Len() int {
+	return 0
+}
+
+func (d *Deque) Less(i, j int) bool {
+	return false
+}
+
+// Swap swaps the elements with indexes i and j.
+func (d *Deque) Swap(i, j int) {
+
+}
