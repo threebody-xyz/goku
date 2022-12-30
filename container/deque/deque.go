b8b6fba480cb86f450f5edeece672429371e4f72
mikatong <mikatong@tencent.com>
2022-12-30 15:17:39 +0800

    feat: support deque

--- container/deque/deque.go
+++ container/deque/deque.go
@@ -2,43 +2,108 @@ package deque
 
 type Element interface {}
 
+const minCapacity = 16
+
 type Deque struct {
+	buffer []Element
+	head int
+	tail int
+	len int
+}
+
+// NewDeque params[0] refer capacity
+func NewDeque(params ...int) *Deque{
+	var capacity int
+	if len(params) > 0 {
+		capacity = params[0]
+	} else {
+		capacity = minCapacity
+	}
+	buffer := make([]Element, capacity)
+	return &Deque{
+		buffer: buffer,
+	}
+}
+
+func (d *Deque) resize() {
+	newBuffer := make([]Element, d.len<<1)
+	if d.tail > d.head {
+		copy(newBuffer, d.buffer[d.head:d.tail])
+	} else {
+		n := copy(newBuffer, d.buffer[d.head:])
+		copy(newBuffer[n:], d.buffer[:d.tail])
+	}
+	d.head = 0
+	d.tail = d.len
+	d.buffer = newBuffer
 }
 
 // PushFront inserts a new element e with value v at the front of deque d.
 func (d *Deque) PushFront(v Element)  {
+	if len(d.buffer) != 0 && d.len == len(d.buffer) {
+		d.resize()
+	}
+	d.head = (d.head - 1) & (len(d.buffer)-1)
+	d.buffer[d.head] = v
+	d.len++
 }
 
 // PushBack inserts a new element e with value v at the back of deque d.
 func (d *Deque) PushBack(v Element)  {
+	if len(d.buffer) != 0 && d.len == len(d.buffer) {
+		d.resize()
+	}
+	d.tail = (d.tail+1) & len(d.buffer)
+	d.buffer[d.tail] = v
+	d.len++
 }
-// Front returns the first element of deque d or nil if the deque is empty.
-func (d *Deque) Front() Element {
-	return nil
+// PopFront returns the first element of deque d or nil if the deque is empty.
+func (d *Deque) PopFront() Element {
+	if d.len <= 0{
+		return nil
+	}
+	result := d.buffer[d.head]
+	var zero Element
+	d.buffer[d.head] = zero
+	d.head =(d.head+1) % len(d.buffer)
+	d.len --
+	if len(d.buffer) > minCapacity && len(d.buffer) == d.len<<2 {
+		d.resize()
+	}
+	return result
 }
 
-// Back returns the last element of deque d or nil if the deque is empty.
-func (d *Deque) Back() Element {
-	return nil
+// PopBack returns the last element of deque d or nil if the deque is empty.
+func (d *Deque) PopBack() Element {
+	if d.len <= 0{
+		return nil
+	}
+	result := d.buffer[d.tail]
+	var zero Element
+	d.buffer[d.tail] = zero
+	d.tail =(d.tail-1) & (len(d.buffer)-1)
+	d.len --
+	if len(d.buffer) > minCapacity && len(d.buffer) == d.len<<2 {
+		d.resize()
+	}
+	return result
 }
 
 func (d *Deque) Get(i int) Element {
-	return nil
+	if i < 0 || i >= d.len {
+		return nil
+	}
+	return d.buffer[(d.head+1)%(len(d.buffer))]
 }
 
 func (d *Deque) Set(i int, v Element) {
+	if i < 0 || i >= d.len {
+		return
+	}
+	d.buffer[(d.head+1)%(len(d.buffer))] = v
 }
 
 // Len returns the number of elements of deque d.
 func (d *Deque) Len() int {
-	return 0
-}
-
-func (d *Deque) Less(i, j int) bool {
-	return false
-}
-
-// Swap swaps the elements with indexes i and j.
-func (d *Deque) Swap(i, j int) {
-
+	return d.len
 }
--- container/deque/deque_test.go
+++ container/deque/deque_test.go
@@ -1,1 +1,67 @@
 package deque
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDeque(t *testing.T){
+	deque := NewDeque()
+	for i:=0; i<minCapacity+1; i++ {
+		deque.PushFront(i)
+	}
+	fmt.Printf("deque: %+v\n", deque)
+
+	for i:=0; i<minCapacity+1; i++ {
+		fmt.Println(deque.PopFront())
+	}
+	fmt.Printf("deque: %+v\n", deque)
+
+	for i:=0; i<minCapacity+1; i++ {
+		deque.PushBack(i)
+	}
+	for i:=0; i<minCapacity+1; i++ {
+		fmt.Println(deque.PopBack())
+	}
+	for i:=0; i<(minCapacity+1)/2; i++ {
+		deque.PushFront(i)
+	}
+	for i:=0; i<(minCapacity+1)/2; i++ {
+		deque.PushBack(i)
+	}
+	fmt.Printf("deque: %+v\n", deque)
+	dequeItem0 := deque.Get(0)
+	fmt.Println(dequeItem0)
+	deque.Set(0, 999)
+	dequeItem0 = deque.Get(0)
+	fmt.Println(dequeItem0)
+}
+
+func BenchmarkPushFront(b *testing.B) {
+	deque := NewDeque()
+	for i:=0; i<b.N; i++ {
+		deque.PushFront(i)
+	}
+}
+
+func BenchmarkPushBack(b *testing.B) {
+	deque := NewDeque()
+	for i:=0; i<b.N; i++ {
+		deque.PushBack(i)
+	}
+}
+
+func BenchmarkPushPopBack(b *testing.B) {
+	deque := NewDeque()
+	for i:=0; i<b.N; i++ {
+		deque.PushBack(i)
+		deque.PopBack()
+	}
+}
+func BenchmarkPushPopFront(b *testing.B) {
+	deque := NewDeque()
+	for i:=0; i<b.N; i++ {
+		deque.PushFront(i)
+		deque.PopFront()
+	}
+}
